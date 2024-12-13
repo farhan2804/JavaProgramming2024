0b5774f1976e5aa225f1391d8469d29a6a9e5ca7
farhan <el3317.farhanmahmood@gmail.com>
2024-12-13 19:10:13 +0530

    sorted duplicate elements

---
+++ removeDupSorted.java
@@ -0,0 +1,20 @@
+//  input- [3,6,3,7,6,3,7]
+//  output-[3,3,3,6,6,7,7,]
+class removeDupSorted {
+    public static void main(String args[]) {
+        int arr[] = { 3, 6, 3, 3, 6, 7, 7 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
